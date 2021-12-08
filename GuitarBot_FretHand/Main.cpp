a9ce4155b17e9dfca47962bd4dee2f0a870e8c7b
nyam3-gatech <80873305+nyam3-gatech@users.noreply.github.com>
2021-12-08 16:19:50 -0500

    Added motor testing code

--- GuitarBot_FretHand/Main.cpp
+++ GuitarBot_FretHand/Main.cpp
@@ -2,43 +2,93 @@
 
 #include "MIDI_Reader.h"
 #include "GuitarTrack.h"
+#include "MotorInterface.h"
+
+#include <iostream>
 
 using namespace std;
 
 int main()
 {
-	MIDI_Reader m("midis/Gil's Music 3 min.mid");
+	//MIDI_Reader m("midis/Gil's Music 3 min.mid");
+	//MIDI_Reader m("midis/tempotest.mid");
 	//MIDI_Reader m("midis/Jumbo Music.mid");
 	//MIDI_Reader m("midis/Gil's Reggae.mid");
 
 	//cout << m.toString();
 
-	GuitarTrack gTrack(m);
+	//GuitarTrack gTrack(m);
 
-	cout << gTrack.toString();
+	//cout << gTrack.toString();
 
-	// EXAMPLE - Accessing Chord Data
+	MotorInterface motors;
+	int control = 0;
+	// current = 200 for normal press, 50 for damped
+	int current = 200;
 
-	vector<ChordEvent*>& chords = gTrack.getChords();
-
-	chords[0]->getTick();
-	chords[0]->getDirection();
-	chords[0]->getTechnique();
-
-	// Real time communication with Arduino
-	
-	//string port = ""; // NEED TO ADD COM PORT
-
-	/*
-	SerialInterface arduino(port);
-
-	if (arduino.isConnected())
+	if (motors.getStatus())
 	{
-		gTrack.run();
+		cout << "Motor setup successful." << endl;
+
+#define ESC_ASCII_VALUE                 0x1b
+
+		for (int id = 0; id < 6; id++)
+		{
+			motors.disableTorque(id);
+			motors.setPositionControl(id);
+			motors.enableTorque(id);
+			motors.setGoalPosition(id, 1800);
+		}
+
+		int input;
+		bool flags[6] = { 0,0,0,0,0,0};
+
+		while (current)
+		{
+			do
+			{
+				cout << "Motor ID: ";
+				cin >> input;
+				if (input < 6 && input >= 0) flags[input] = 1;
+			}
+			while (input < 6 && input >= 0);
+			cout << "Current: ";
+			cin >> current;
+			cout << "Set to current control." << endl;
+			for (int id = 0; id < 6; id++)
+			{
+				if (flags[id])
+				{
+					motors.disableTorque(id);
+					motors.setCurrentControl(id);
+					motors.enableTorque(id);
+					motors.setGoalCurrent(id, current);
+					flags[id] = 0;
+				}
+				else
+				{
+					motors.disableTorque(id);
+					motors.setPositionControl(id);
+					motors.enableTorque(id);
+					motors.setGoalPosition(id, 1800);
+				}
+			}
+		}
+
+		for (int id = 0; id < 6; id++)
+		{
+			motors.disableTorque(id);
+			motors.setPositionControl(id);
+			motors.enableTorque(id);
+			motors.setGoalPosition(id, 1800);
+			motors.disableTorque(id);
+		}
+
 	}
 	else
 	{
-		cout << "Error: Arduino not connected." << endl;
+		cout << "Motor setup failed." << endl;
 	}
-	*/
+
+
 }
