d91df6b6c6a5f92d2f9852546a845824d2197e7c
nyam3-gatech <80873305+nyam3-gatech@users.noreply.github.com>
2022-12-07 00:36:19 -0500

    Prototype code for robot control

--- GBotPython/GBotPython.py
+++ GBotPython/GBotPython.py
@@ -1,19 +1,29 @@
 import GBotData as gbd
+import time
 
 print(gbd.module_test())
 
-gTrack = gbd.GuitarTrack("midis/strings.mid")
+gTrack = gbd.GuitarTrack("midis/Gil's Music 3 min.mid")
+gCmdArray = gbd.genGuitarCmdArray(gTrack)
 
-for index, chord in enumerate(gTrack.getChords()):
-
-    print("Chord Index:")
-    print(index)
-    print("LH Play Command:")
-    print(gbd.genPlayCommand(chord))
-    print("RH Move Command:")
-    print(gbd.genRHMoveCommand(gTrack, index).toString())
-    print("RH Play Command:")
-    print(gbd.genRHPlayCommand(chord).toString())
-
-
+numCmds = len(gCmdArray)
+cmdIndex = 0
+time_stamp = -1.0
+t_init = time.time()
+songLength = gTrack.getTrackLength() + 1
+endOfSong = t_init + songLength
+while (cmdIndex < numCmds) and (time_stamp < songLength):
+    gCmd = gCmdArray[cmdIndex]
+    time_stamp = time.time() - t_init - 1
+    if (time_stamp) >= gCmd.time:
+        print("t = " + str(time_stamp))
+        print("  is_sound = " + str(gCmd.is_sound));
+        if(gCmd.moveLH):
+            # move LH
+            print("  moveLH")
+        if(gCmd.moveRH):
+            # move RH
+            print("  moveRH")
+        cmdIndex += 1
+        print("  tn = " + str(gCmdArray[cmdIndex].time));
 
